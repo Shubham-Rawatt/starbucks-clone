13e2c0cc29962f64d3b8561ffb6c3dafcdf63764
Shubham Rawat <shubhamrawat97614@gmail.com>
2026-08-24 11:48:19 +0530

    Update recommendation components

--- src/components/recommend/recommend.tsx
+++ src/components/recommend/recommend.tsx
@@ -1,3 +1,63 @@
+// "use client";
+
+// import RecommendedCard from "../recommendCard/recommendcard";
+// import { RecommendData } from "@/data/recommendData";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation } from "swiper/modules";
+// import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+// import "swiper/css";
+
+
+// const Recommended = () => {
+//   return (
+//     <section className="mt-10">
+//       <h5 className="text-[#00754a] font-bold px-36 text-md">Recommended</h5>
+//       <div className="bg-[#f2f0eb] px-10 py-10">
+//         <div className="relative mx-auto max-w-6xl">
+
+//           {/* Left Button */}
+//           <button
+//             className="recommended-prev absolute -left-1 top-1/2 z-10
+//             flex h-9 w-9 -translate-y-1/2 items-center justify-center
+//             rounded-full bg-white shadow-lg
+//             transition-transform duration-300 hover:scale-105"
+//           >
+//             <FiChevronLeft className="text-xl text-[#00754a]" />
+//           </button>
+
+//           {/* Right Button */}
+//           <button
+//             className="recommended-next absolute -right-1 top-1/2 z-10
+//             flex h-9 w-9 -translate-y-1/2 items-center justify-center
+//             rounded-full bg-white shadow-lg
+//             transition-transform duration-300 hover:scale-105"
+//           >
+//             <FiChevronRight className="text-xl text-[#00754a]" />
+//           </button>
+
+//           <Swiper
+//             modules={[Navigation]}
+//             navigation={{
+//               prevEl: ".recommended-prev",
+//               nextEl: ".recommended-next",
+//             }}
+//             spaceBetween={20}
+//             slidesPerView={3}
+//           >
+//             {RecommendData.map((item) => (
+//               <SwiperSlide key={item.id}>
+//                 <RecommendedCard RecommendData={item} />
+//               </SwiperSlide>
+//             ))}
+//           </Swiper>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Recommended;
+
 "use client";
 
 import RecommendedCard from "../recommendCard/recommendcard";
@@ -5,18 +65,23 @@ import { RecommendData } from "@/data/recommendData";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
-import "swiper/css";
 
+import "swiper/css";
+import "swiper/css/navigation";
 
 const Recommended = () => {
   return (
     <section className="mt-10">
-      <h5 className="text-[#00754a] font-bold px-36 text-md">Recommended</h5>
+      <h5 className="px-36 text-md font-bold text-[#00754a]">
+        Recommended
+      </h5>
+
       <div className="bg-[#f2f0eb] px-10 py-10">
         <div className="relative mx-auto max-w-6xl">
 
-          {/* Left Button */}
+          {/* Previous Button */}
           <button
+            type="button"
             className="recommended-prev absolute -left-1 top-1/2 z-10
             flex h-9 w-9 -translate-y-1/2 items-center justify-center
             rounded-full bg-white shadow-lg
@@ -25,8 +90,9 @@ const Recommended = () => {
             <FiChevronLeft className="text-xl text-[#00754a]" />
           </button>
 
-          {/* Right Button */}
+          {/* Next Button */}
           <button
+            type="button"
             className="recommended-next absolute -right-1 top-1/2 z-10
             flex h-9 w-9 -translate-y-1/2 items-center justify-center
             rounded-full bg-white shadow-lg
@@ -50,6 +116,7 @@ const Recommended = () => {
               </SwiperSlide>
             ))}
           </Swiper>
+
         </div>
       </div>
     </section>
--- src/components/recommendCard/recommendcard.tsx
+++ src/components/recommendCard/recommendcard.tsx
@@ -1,55 +1,117 @@
+// "use client";
+// import Image from "next/image";
+// import { FiPlus } from "react-icons/fi";
+
+// //  isme data show hoga
+// interface Data {
+//   id: number;
+//   name: string;
+//   calory:string;
+//   price:string;
+//   image1: string;
+//   image2: string;
+//   // icon: string;
+// }
+
+
+// const RecommendedCard = ({ RecommendData } : {RecommendData : Data }) => {
+//   return (
+//     <div className="w-sm rounded-xl bg-white p-5 shadow-lg">
+// {/* img first  */}
+// <div className="flex gap-4">
+
+//       <Image
+//         src={RecommendData.image1}
+//         alt={RecommendData.name}
+//         width={80}
+//         height={80}
+//         className="rounded-full"
+//       />
+
+//     {/* img sec */}
+//     <div>
+//        <Image
+//         src={RecommendData.image2}
+//         alt={RecommendData.name}
+//         width={16}
+//         height={16}
+//         className=""
+//         />
+//       <h3 className="mt-2 font-bold text-sm text-[#000000]"> {RecommendData.name} </h3>
+//       <p className=" text-xs text-[#212529bf] font-bold">{RecommendData.calory}</p>
+//     </div>
+
+// </div>
+//       <div className="mt-3 flex items-center font-bold text-md justify-between">
+//         <p>{RecommendData.price}</p>
+
+//         <button className="flex h-8 w-8 items-center justify-center rounded-full bg-[#00754A] text-white" onClick={()=> alert('item added')}>
+//           <FiPlus  />
+//         </button>
+//       </div>
+
+//     </div>
+//   );
+// };
+
+// export default RecommendedCard;
+
+
 "use client";
+
 import Image from "next/image";
 import { FiPlus } from "react-icons/fi";
 
-//  isme data show hoga
 interface Data {
   id: number;
   name: string;
-  calory:string;
-  price:string;
+  calory: string;
+  price: string;
   image1: string;
   image2: string;
-  icon: string;
 }
 
-
-const RecommendedCard = ({ RecommendData } : {RecommendData : Data }) => {
+const RecommendedCard = ({ RecommendData }: { RecommendData: Data }) => {
   return (
     <div className="w-sm rounded-xl bg-white p-5 shadow-lg">
-{/* img first  */}
-<div className="flex gap-4">
-
-      <Image
-        src={RecommendData.image1}
-        alt={RecommendData.name}
-        width={80}
-        height={80}
-        className="rounded-full"
-      />
-
-    {/* img sec */}
-    <div>
-       <Image
-        src={RecommendData.image2}
-        alt={RecommendData.name}
-        width={16}
-        height={16}
-        className=""
+      <div className="flex gap-4">
+        <Image
+          src={RecommendData.image1}
+          alt={RecommendData.name}
+          width={80}
+          height={80}
+          className="rounded-full"
         />
-      <h3 className="mt-2 font-bold text-sm text-[#000000]"> {RecommendData.name} </h3>
-      <p className=" text-xs text-[#212529bf] font-bold">{RecommendData.calory}</p>
-    </div>
 
-</div>
-      <div className="mt-3 flex items-center font-bold text-md justify-between">
-        <p>{RecommendData.price}</p>
+        <div>
+          <Image
+            src={RecommendData.image2}
+            alt={RecommendData.name}
+            width={16}
+            height={16}
+          />
 
-        <button className="flex h-8 w-8 items-center justify-center rounded-full bg-[#00754A] text-white" onClick={()=> alert('item added')}>
-          <FiPlus  />
-        </button>
+          <h3 className="mt-2 text-sm font-bold text-[#000000]">
+            {RecommendData.name}
+          </h3>
+
+          <p className="text-xs font-bold text-[#212529bf]">
+            {RecommendData.calory}
+          </p>
+        </div>
       </div>
 
+      <div className="mt-3 flex items-center justify-between text-md font-bold">
+        <p>{RecommendData.price}</p>
+
+        <button
+          type="button"
+          className="flex h-8 w-8 items-center justify-center rounded-full bg-[#00754A] text-white"
+          onClick={() => alert("item added")}
+        >
+          <FiPlus />
+        </button>
+      </div>
     </div>
   );
 };
